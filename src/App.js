ebfb89c905da245e0cbb6b585d4d703e8c1b3a4d
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-03-19 01:13:04 +0200

    Added basic files for main pages. Added Route and Switch.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+
+import HomePage from './views/HomePage';
+import MoviesPage from './views/MoviesPage';
+import MovieDetailsPage from './views/MovieDetailsPage';
+import NotFound from './views/NotFound';
+
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 class App extends Component {
   state = {};
   render() {
     return (
-      <div>
-        <h1>Hello, I am future Home Task 4</h1>
-      </div>
+      <Fragment>
+        <nav>
+          <ul>
+            <li>
+              <NavLink
+                exact
+                to="/"
+                className="NavLink"
+                activeClassName="NavLink-active"
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/movies"
+                className="NavLink"
+                activeClassName="NavLink-active"
+              >
+                Movies
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/movies" component={MoviesPage} />
+          <Route path="/movies/:movieId" component={MovieDetailsPage} />
+          <Route path="/movies/:movieId/cast" component={MovieDetailsPage} />
+          <Route path="/movies/:movieId/reviews" component={MovieDetailsPage} />
+          <Route component={NotFound} />
+        </Switch>
+      </Fragment>
     );
   }
 }
---
+++ src/views/MovieDetailsPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MovieDetailsPage = () => {
+  return (
+    <div>
+      <h1>Movies Details Page</h1>
+    </div>
+  );
+};
+
+export default MovieDetailsPage;
