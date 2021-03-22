0026c925967dc9ad3b9d09b8c0518c8d5d844097
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-03-22 21:39:12 +0200

    Added lazy imports for views.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 // React imports
-import React, { Component } from 'react';
+import React, { Component, Suspense, lazy } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 // Views imports
-import HomePage from './views/HomePage';
-import MoviesPage from './views/MoviesPage';
-import MovieDetailsPage from './views/MovieDetailsPage';
+// import HomePage from './views/HomePage';
+// import MoviesPage from './views/MoviesPage';
+// import MovieDetailsPage from './views/MovieDetailsPage';
 import NotFound from './views/NotFound';
 
 // Components imports
 import AppBar from './components/AppBar';
-import Footer from './components/Footer';
+// import Footer from './components/Footer';
 
 // Routes imports
 import routes from './routes';
+
+// Lazy loading for views
+const HomePage = lazy(() =>
+  import('./views/HomePage.js' /* webpackChunkName: "home-page" */),
+);
+const MoviesPage = lazy(() =>
+  import('./views/MoviesPage.js' /* webpackChunkName: "movies-page" */),
+);
+const MovieDetailsPage = lazy(() =>
+  import('./views/MovieDetailsPage.js' /* webpackChunkName: "details-page" */),
+);
 
 class App extends Component {
   state = {};
@@ -21,12 +32,14 @@ class App extends Component {
     return (
       <>
         <AppBar />
-        <Switch>
-          <Route exact path={routes.home} component={HomePage} />
-          <Route exact path={routes.movieSearch} component={MoviesPage} />
-          <Route path={routes.movieDetails} component={MovieDetailsPage} />
-          <Route component={NotFound} />
-        </Switch>
+        <Suspense fallback="...Loading">
+          <Switch>
+            <Route exact path={routes.home} component={HomePage} />
+            <Route exact path={routes.movieSearch} component={MoviesPage} />
+            <Route path={routes.movieDetails} component={MovieDetailsPage} />
+            <Route component={NotFound} />
+          </Switch>
+        </Suspense>
         {/* <Footer /> */}
       </>
     );
