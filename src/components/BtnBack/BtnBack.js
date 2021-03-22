e9dd43b8639368b04cd5d7634fc7bd645f7e4905
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-03-22 20:55:11 +0200

    Added styles for Return button.

--- src/components/BtnBack/BtnBack.js
+++ src/components/BtnBack/BtnBack.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import styles from './BtnBack.module.css';
+
 const BtnBack = ({ history }) => {
-  return <button onClick={history.goBack}>Back</button>;
+  return (
+    <button onClick={history.goBack} className={styles.Button}>
+      Return
+    </button>
+  );
 };
 
 export default BtnBack;
