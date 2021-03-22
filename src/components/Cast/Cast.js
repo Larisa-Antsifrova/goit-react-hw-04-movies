8b7b303ebb1f3d0236cb67ff91da3fb15b2f32dc
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-03-22 22:22:56 +0200

    Added basic style to Cast list.

--- src/components/Cast/Cast.js
+++ src/components/Cast/Cast.js
@@ -2,11 +2,19 @@ import React from 'react';
 import styles from './Cast.module.css';
 
 const Cast = ({ cast }) => {
+  console.log(cast);
   return (
     <div>
       <ul className={styles.castList}>
         {cast.map(actor => (
-          <li key={actor.id}>{actor.name}</li>
+          <li key={actor.id} className={styles.item}>
+            <img
+              className={styles.avatar}
+              src={`https://image.tmdb.org/t/p/w500/${actor.profile_path}`}
+              alt="User avatar"
+            />
+            <p className={styles.name}>{actor.name}</p>
+          </li>
         ))}
       </ul>
     </div>
