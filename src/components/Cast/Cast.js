119b4e15c7611f38ead16bcbf72bb9250990b71b
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-03-22 22:16:48 +0200

    Edited ExtraInfoBar.

--- src/components/Cast/Cast.js
+++ src/components/Cast/Cast.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from './Cast.module.css';
 
 const Cast = ({ cast }) => {
   return (
     <div>
-      <ul>
+      <ul className={styles.castList}>
         {cast.map(actor => (
           <li key={actor.id}>{actor.name}</li>
         ))}
