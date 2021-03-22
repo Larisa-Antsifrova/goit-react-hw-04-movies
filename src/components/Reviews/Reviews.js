b89ab7b0d20fe8b6c53f70348f769dbac68dd3ba
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-03-22 22:34:45 +0200

    Added basic styles to Reviews.

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,21 +1,25 @@
 import React from 'react';
 
+import styles from './Reviews.module.css';
+
 const Reviews = ({ reviews }) => {
   return (
-    <div>
+    <>
       <ul>
         {reviews.map(review => (
-          <li key={review.id}>
-            <img
-              src={review.author_details.avatar_path.slice(1)}
-              alt={review.author}
-            />
-            <p>{review.author}</p>
+          <li key={review.id} className={styles.ReviewCard}>
+            <div className={styles.imageSection}>
+              <img
+                src={review.author_details.avatar_path.slice(1)}
+                alt={review.author}
+              />
+              <p>{review.author}</p>
+            </div>
             <p>{review.content}</p>
           </li>
         ))}
       </ul>
-    </div>
+    </>
   );
 };
 
